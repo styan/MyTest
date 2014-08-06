822c2d2be43c1dfaa026916e9fb78e32fd8890ad
styan <yan314@gmail.com>
2014-08-06 17:04:33 +0800

    test

--- app/src/main/java/com/example/administrator/myapplication/MyActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MyActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.myapplication;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,10 +11,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-
+import android.widget.EditText;
 
 
 public class MyActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "com.example.administrator.myapplication.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,5 +63,14 @@ public class MyActivity extends Activity {
             View rootView = inflater.inflate(R.layout.fragment_my, container, false);
             return rootView;
         }
+    }
+
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
